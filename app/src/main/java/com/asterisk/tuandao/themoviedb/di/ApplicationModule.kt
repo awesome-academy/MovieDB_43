07c7333de00264d76713975cb7c7eaf3b58824aa
tuandao2511 <anhdt251196@gmail.com>
2019-06-03 13:26:44 +0700

    12746 create ui home screen

--- app/src/main/java/com/asterisk/tuandao/themoviedb/di/ApplicationModule.kt
+++ app/src/main/java/com/asterisk/tuandao/themoviedb/di/ApplicationModule.kt
@@ -19,6 +19,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+
 @Module(includes = [MainViewModelModule::class])
 abstract class ApplicationModule {
 
