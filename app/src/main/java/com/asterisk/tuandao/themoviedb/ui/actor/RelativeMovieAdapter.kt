911c6b05ad143a50b9d38740c9e7ff022d132fb8
tuandao2511 <anhdt251196@gmail.com>
2019-06-07 23:13:44 +0700

    12762 handle input data search screen

---
+++ app/src/main/java/com/asterisk/tuandao/themoviedb/ui/actor/RelativeMovieAdapter.kt
@@ -0,0 +1,47 @@
+package com.asterisk.tuandao.themoviedb.ui.actor
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.asterisk.tuandao.themoviedb.R
+import com.asterisk.tuandao.themoviedb.data.source.model.Actor
+import com.asterisk.tuandao.themoviedb.data.source.model.Movie
+import com.asterisk.tuandao.themoviedb.databinding.ItemActorRelatedMovieBinding
+
+class RelativeMovieAdapter(
+    private var movies: List<Movie>,
+    val actorViewModel: ActorViewModel
+) : RecyclerView.Adapter<RelativeMovieAdapter.RelativeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RelativeViewHolder {
+        return RelativeViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context), R.layout.item_actor, parent, false), actorViewModel)
+    }
+
+    override fun getItemCount() = movies.size
+
+    fun swapAdapter(newCasts: List<Movie>) {
+        movies = newCasts
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: RelativeViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bindView(movie)
+    }
+
+    class RelativeViewHolder(
+        val binding: ItemActorRelatedMovieBinding,
+        actorViewModel: ActorViewModel
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.viewmodel = actorViewModel
+        }
+
+        fun bindView(data: Movie?) {
+            binding.movie = data
+        }
+    }
+}
